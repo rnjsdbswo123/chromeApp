2e7f8733b5ceab646462a7a07d9d47631dd599be
rnjsdbswo123 <64054467+rnjsdbswo123@users.noreply.github.com>
2022-11-12 16:26:43 +0900

    todo진행 중

---
+++ js/todo.js
@@ -0,0 +1,42 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input")
+const toDoList = document.getElementById("todo-list");
+
+const toDos = [];
+const TODOS_KEY ="todos"
+function deleteToDo(event){
+    const li = (event.target.parentElement);
+    li.remove()
+}
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    span.innerText = newTodo;
+    button.innerText = "⛔";
+    button.addEventListener("click",deleteToDo)
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value="";
+    toDos.push(newTodo)
+    paintToDo(newTodo)
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log("this is the turn of", item));
+}
